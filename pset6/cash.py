93f8b0e1c5bc97a99c9030e8febe744a1f8e7189
Yori <yori.verbist1212@gmail.com>
2019-07-19 20:07:59 +0200

    cash.py gemaakt

--- pset6/cash.py
+++ pset6/cash.py
@@ -1,7 +1,10 @@
 from cs50 import get_float
 
 def main():
-    change = get_float("What is the change?")
+    change = -5
+    
+    while change < 0:
+        change = get_float("What is the change?")
     quaters = 0
     dimes = 0
     nickels = 0
@@ -10,18 +13,22 @@ def main():
     while change >= 0.25:
         change -= 0.25
         quaters += 1
+        print("q", quaters)
 
     while change >= 0.10:
         change -= 0.10
         dimes += 1
+        print("d", dimes)
     
     while change >= 0.05:
         change -= 0.05
         nickels += 1
+        print("n", nickels)
         
     while change >= 0.01:
         change -= 0.01
         pennies += 1
+        print("p", pennies)
         
     print(quaters + dimes + nickels + pennies)
     
