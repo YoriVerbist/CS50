82ca160a77cafcb9d44eef057b2f9e1db2ae565e
Yori <yori.verbist1212@gmail.com>
2019-07-25 10:28:09 +0200

    app.py gestart

--- pset7/Similarities/helpers.py
+++ pset7/Similarities/helpers.py
@@ -1,15 +1,20 @@
+from nltk.tokenize import sent_tokenize
+
+
 def lines(a, b):
     """Return lines in both a and b"""
 
-    # TODO
-    return []
+    line_a = a.split(sep="\n")
+    line_b = b.split(sep="\n")
+    return [line_a, line_b]
 
 
 def sentences(a, b):
     """Return sentences in both a and b"""
 
-    # TODO
-    return []
+    sentence_a = sent_tokenize(a)
+    sentence_b = sent_tokenize(b)
+    return [sentence_a, sentence_b]
 
 
 def substrings(a, b, n):
