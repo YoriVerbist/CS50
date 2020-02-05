34c1199f480566be8108749f13fbc23504b27565
Yori <yori.verbist1212@gmail.com>
2020-02-05 11:38:01 +0100

    tabulate function finished

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -145,6 +145,17 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
+    for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (candidates[preferences[i][j]].eliminated == false)
+            {
+                candidates[preferences[i][j]].votes += 1;
+                break;
+            }
+        }
+    }
     
     return;
 }
