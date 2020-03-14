8586f9a9b31eaa1bfa394855492e3e7cedd5f27d
Yori <yori.verbist1212@gmail.com>
2020-03-14 12:47:45 +0100

    aanpassingen recover.c

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -5,15 +5,18 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Give only 1 argument (image name)");
+        printf("Give only 1 argument (image name)\n");
         return 1;
     }
-    
-    FILE *file = fopen(argv[1], "r");
+    // Getting file name from command line
+    char *infile = argv[1];
+
+    // Opening the file
+    FILE *file = fopen(infile, "r");
     // Check if the file exists
     if (file == NULL)
     {
-        fprintf(stderr, "Could not open %s.\n", file);
+        fprintf(stderr, "Could not open %s.\n", infile);
         return 1;
     }
 }
