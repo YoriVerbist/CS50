e20a61ed533242c5760e89b322a45c6aba046247
Yori <yori.verbist1212@gmail.com>
2020-04-02 21:52:23 +0200

    Recover finished

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -23,14 +23,30 @@ int main(int argc, char *argv[])
 
     typedef uint8_t BYTE;
     BYTE buffer[512];
+    
+    FILE *img = NULL; // NULL because there hasn't been a jpeg file yet.
+    char filename[8]; // The length of the filename we write to
+    int fileCounter = 0; // Check which file we are at
 
-    while (fread(buffer, 512, 1,file))
+    while (fread(buffer, 512, 1, file)) // fread returns number of items of size 512 were read.
     {
+        BYTE n = printf("%i\n", buffer[0]);
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) // Last check checks only for 0xe and doesn't care about the number after that.
         {
-            
+            if (fileCounter != 0)
+            {
+                fclose(img); // Close the previous jpg file before opening a new one
+            }
+            sprintf(filename, "%03i.jpg", fileCounter); // %03i == print int with 3 digits
+            img = fopen(filename, "w");
+            fileCounter += 1;
         }
-        
+        if (fileCounter != 0)
+        {
+            fwrite(buffer, 512, 1, img);
+        }
     }
+    fclose(img);
+    fclose(file);
     
 }
