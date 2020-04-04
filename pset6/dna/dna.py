e34811d8489987678a0344280b0095937b737189
Yori <yori.verbist1212@gmail.com>
2020-04-04 13:52:33 +0200

    updates

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -9,22 +9,40 @@ def main():
         txtName = sys.argv[2]
         STRs = dict()
         with open(csvName, 'r') as csvFile:
-            lines1 = csvFile.readlines()
-            names = lines1[0].split(",")
+            lines = csvFile.readlines()
+            names = lines[0].split(",")
             names.remove(names[0])
             print(names)
             
-            lines1.remove(lines1[0])
-            for line in lines1:
+            lines.remove(lines[0])
+            for line in lines:
                 amount = line.split(",")
         
         with open(txtName, 'r') as txtFile:
-            lines2 = txtFile.read()
+            string = txtFile.read()
+    #print(string, names[0])
+    print(string.count(names[0]))
+    print(calculateOccurrences(string, names[0]))
 
 
 def calculateOccurrences(string, substring):
-    while True:
-        string.find(substring)
+    occurrences = 0
+    highest = 0
+    while string.find(substring) != -1:
+        place = string.find(substring)
+        string = string[place:]
+        if string.find(substring, 0, len(substring)) == 0:
+        #if substring in string[place:place + len(substring)]:
+            occurrences += 1
+            print(occurrences)
+            print("here")
+        else:
+            occurrences = 0
+        if occurrences > highest:
+                print(highest, occurrences)
+                highest = occurrences
+        string = string[1:]
+    return highest
     
     
 if __name__ == "__main__":
